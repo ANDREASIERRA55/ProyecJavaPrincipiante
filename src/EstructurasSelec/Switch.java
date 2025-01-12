69efa206e58d83a5d5d024f72112823328ff6842
Andrea Sierra <andreasierra103@gmail.com>
2025-01-12 10:08:37 +0100

    Creacion de Operador Ternario ?

--- src/EstructurasSelec/Switch.java
+++ src/EstructurasSelec/Switch.java
@@ -7,28 +7,16 @@ class Switch {
 		int dia = 5;
 		String nombreDia;
 		
-		switch (dia) {
-		
-		case 1: nombreDia = "Lunes";
-		break;
-		case 2: nombreDia = "Martes";
-		break;
-		case 3: nombreDia = "Miercoles";
-		break;
-		case 4: nombreDia = "Jueves";
-		break;
-		case 5: nombreDia = "Viernes";
-		break;
-		case 6: nombreDia = "Sabado";
-		break;
-		case 7: nombreDia = "Domingo";
-		break;
-		
-		default:nombreDia = "Nombre dia invalido";
-		break;
-		
-		
-		}
+                nombreDia = switch (dia) {
+                case 1 -> "Lunes";
+                case 2 -> "Martes";
+                case 3 -> "Miercoles";
+                case 4 -> "Jueves";
+                case 5 -> "Viernes";
+                case 6 -> "Sabado";
+                case 7 -> "Domingo";
+                default -> "Nombre dia invalido";
+            };
 		
 		
 		System.out.println("El dia de la semana eleccionado es: " + nombreDia);
