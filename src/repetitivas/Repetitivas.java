8b0e6bf53aa36b074666a5055f38fb1460774c46
Andrea Sierra <andreasierra103@gmail.com>
2025-01-13 17:59:01 +0100

    Creacion de bucle infinito sin ejecutar por precacusion

--- src/repetitivas/Repetitivas.java
+++ src/repetitivas/Repetitivas.java
@@ -1,6 +1,8 @@
 package repetitivas;
 
-public class Repetitivas {
+// Clase Repetitivas Mientras "WHILE"
+
+/*public class Repetitivas {
     public static void main(String[] args) {
         int cont = 0;
         while (cont < 10) {
@@ -9,4 +11,18 @@ public class Repetitivas {
         }
     }
 
-}
+} */
+
+// Estructura Repetitiva Bucles Infinitos ""
+
+
+/*public class Repetitivas {
+    public static void main(String[] args) {
+        boolean centinela = true;
+
+        while (centinela == true) {
+            System.out.println("Estoy en un bucle infinito" + centinela);
+
+        }
+    }
+}*/
