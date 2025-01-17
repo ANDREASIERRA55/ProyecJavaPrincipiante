53b50351a0b8d24dbde220f7b780ccf2c26dac04
Andrea Sierra <andreasierra103@gmail.com>
2025-01-17 17:55:57 +0100

    Entendiendo el for

--- src/repetitivas/Repetitivas.java
+++ src/repetitivas/Repetitivas.java
@@ -26,3 +26,12 @@ package repetitivas;
         }
     }
 }*/
+
+public class Repetitivas {
+    public static void main(String[] args) {
+        for (int cont=0; cont<=10; cont++){
+            System.out.println("Estoy dando vueltas con for, estoy en la vuelta: " + cont);
+        }
+    }
+    
+}
